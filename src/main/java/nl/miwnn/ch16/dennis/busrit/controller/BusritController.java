4f86bc526d273fb17ef2a04eb8c4e83253cc84b5
Dennis Mei <dennismei@MBA-Dennis-Mei.local>
2025-06-06 14:21:49 +0200

    Added bus entitity and the functionality to access the database

--- src/main/java/nl/miwnn/ch16/dennis/busrit/controller/BusritController.java
+++ src/main/java/nl/miwnn/ch16/dennis/busrit/controller/BusritController.java
@@ -1,18 +1,32 @@
 package nl.miwnn.ch16.dennis.busrit.controller;
 
+import nl.miwnn.ch16.dennis.busrit.model.Bus;
+import nl.miwnn.ch16.dennis.busrit.repositories.BusRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class BusritController {
 
-    @GetMapping
+    private final BusRepository busRepository;
+
+    public BusritController(BusRepository busRepository) {
+        this.busRepository = busRepository;
+    }
+
+    @GetMapping("/")
     private String showBusOverview(Model dataModel) {
-        dataModel.
-        dataModel.addAttribute("attribute", "value");
+        dataModel.addAttribute("allBusses", busRepository.findAll());
 
         return "busOverview";
     }
 
+    @PostMapping("/bus/new")
+    private String showNewBusForm(Model dataModel) {
+        return "";
+    }
+
 }
+
--- src/main/java/nl/miwnn/ch16/dennis/busrit/model/Bus.java
+++ src/main/java/nl/miwnn/ch16/dennis/busrit/model/Bus.java
@@ -1,13 +1,54 @@
 package nl.miwnn.ch16.dennis.busrit.model;
 
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
 public class Bus {
+    @Id @GeneratedValue
+    private long busId;
 
-    private int busNumber;
+    private int lijnnummer;
+    private String regio;
+    private int zitplaatsen;
 
-    public Bus(int busNumber) {
-        this.busNumber = busNumber;
+    @Override
+    public String toString() {
+        return String.format("bus %d regio %s- %d zitplaatsen",
+                busId, regio, zitplaatsen);
     }
 
+    public long getBusId() {
+        return busId;
+    }
 
+    public void setBusId(long busId) {
+        this.busId = busId;
+    }
+
+    public int getLijnnummer() {
+        return lijnnummer;
+    }
+
+    public void setLijnnummer(int lijnnummer) {
+        this.lijnnummer = lijnnummer;
+    }
+
+    public String getRegio() {
+        return regio;
+    }
+
+    public void setRegio(String regio) {
+        this.regio = regio;
+    }
+
+    public int getZitplaatsen() {
+        return zitplaatsen;
+    }
+
+    public void setZitplaatsen(int zitplaatsen) {
+        this.zitplaatsen = zitplaatsen;
+    }
 }
