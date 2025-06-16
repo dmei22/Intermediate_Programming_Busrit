a754aafa5d70553d9681af089bf27fedf98ed596
Dennis Mei <dennismei@MBA-Dennis-Mei.local>
2025-06-16 13:29:44 +0200

    Added login functionality with authentication

---
+++ src/main/java/nl/miwnn/ch16/dennis/busrit/config/BusritSecurityConfig.java
@@ -0,0 +1,28 @@
+package nl.miwnn.ch16.dennis.busrit.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class BusritSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .authorizeHttpRequests((request) -> request
+                        .requestMatchers("/", "/bus/overview").permitAll()
+                        .requestMatchers("/traveler/overview").permitAll()
+                        .requestMatchers("/webjars/**", "/css/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(AbstractAuthenticationFilterConfigurer::permitAll)
+                .logout((logout) -> logout.logoutSuccessUrl("/"));
+
+        return httpSecurity.build();
+    }
+}
